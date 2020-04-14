e4ae6047a7d04c10083c575547e55297ab11cd03
hannahwilssens <51299043+hannahwilssens@users.noreply.github.com>
2020-04-14 13:42:15 +0200

    mijn eerste commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+const data = require('./data/info.json');
+
+app.get('/', function(req, res){
+  res.render('homepagebody', {
+
+  });
+});
+
+app.get('/contact', function(req, res){
+  res.render('contact', {
+
+  });
+});
+
+
+
+
+app.get("/tentoonstelling1", function(request, response){  response.render("tentoonstelling1", {
+  infos: data.informatie
+});
+});
+
+// detailpagina van een blogbericht
+app.get('/tentoonstelling1/:postid', function(req,res){
+  res.render('detail', {
+    info: data.informatie[req.params.postid]
+  });
+});
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
---
+++ public/js/script2.js
@@ -0,0 +1,5 @@
+let mijnMenu = document.querySelector(".menu");
+let mijnNav = document.querySelector("nav");
+mijnMenu.addEventListener("click", function() {
+  mijnNav.classList.toggle("zichtbaar");
+});
