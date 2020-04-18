c8166d6989f30ec3de7489c5f09402d028ef4fab
hannahwilssens <51299043+hannahwilssens@users.noreply.github.com>
2020-04-18 12:34:32 +0200

    Pagina's bijgewerkt

--- public/js/script2.js
+++ public/js/script2.js
@@ -3,3 +3,47 @@ let mijnNav = document.querySelector("nav");
 mijnMenu.addEventListener("click", function() {
   mijnNav.classList.toggle("zichtbaar");
 });
+
+
+var iso = new Isotope( '.raster', {
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+});
+
+var qsRegex;
+
+
+var $raster = $('.raster').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+  filter: function() {
+    return qsRegex ? $(this).text().match( qsRegex ) : true;
+  },
+  getSortData: {
+      category: '[data-category]',
+  }
+});
+
+
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll('.button-group');
+for ( var i=0; i < buttonGroups.length; i++ ) {
+  buttonGroups[i].addEventListener( 'click', onButtonGroupClick );
+}
+
+function onButtonGroupClick( event ) {
+  // only button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var button = event.target;
+  button.parentNode.querySelector('.is-checked').classList.remove('is-checked');
+  button.classList.add('is-checked');
+}
